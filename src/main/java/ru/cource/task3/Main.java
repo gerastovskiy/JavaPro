bf1fcd28e9ca7d326896f8d7420ee401f128a90a
Алексей Герастовский <gerastovskii@vtb.ru>
2024-10-31 00:09:32 +0300

    task 3

---
+++ src/main/java/ru/cource/task3/Main.java
@@ -0,0 +1,52 @@
+package ru.cource.task3;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+Попробуйте реализовать собственный пул потоков. В качестве аргументов конструктора пулу передается его емкость (количество рабочих потоков).
+Как только пул создан, он сразу инициализирует и запускает потоки. Внутри пула очередь задач на исполнение организуется через LinkedList<Runnable>.
+При выполнении у пула потоков метода execute(Runnabler), указанная задача должна попасть в очередь исполнения, и как только появится свободный поток – должна быть выполнена.
+Также необходимо реализовать метод shutdown(), после выполнения которого новые задачи больше не принимаются пулом (при попытке добавить задачу можно бросать IllegalStateException),
+и все потоки для которых больше нет задач завершают свою работу.
+Дополнительно можно добавить метод awaitTermination() без таймаута, работающий аналогично стандартным пулам потоков.
+ */
+public class Main {
+    private static AtomicInteger taskCounter = new AtomicInteger(1);
+
+    public static Runnable newRunnable(long delay){
+        Runnable r = () ->
+        {
+            var taskCount = taskCounter.getAndIncrement();
+
+            System.out.println(String.format("Задание %s поставлено в очередь", taskCount));
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(String.format("Задание %s завершено", taskCount));
+        };
+
+        return r;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var pool = new MyPoolExecutor(3);
+
+        pool.execute(newRunnable(1000L));
+        pool.execute(newRunnable(6000L));
+        pool.execute(newRunnable(3000L));
+        pool.execute(newRunnable(5000L));
+        pool.execute(newRunnable(1000L));
+        pool.execute(newRunnable(500L));
+        pool.execute(newRunnable(100L));
+
+        //Thread.sleep(2000L);
+        pool.shutdown();
+        //pool.shutdownNow();
+
+        pool.execute(newRunnable(1));
+
+        System.out.println("This is the END");
+    }
+}
---
+++ src/main/java/ru/cource/task3/MyPoolExecutor.java
@@ -0,0 +1,72 @@
+package ru.cource.task3;
+
+import java.util.*;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MyPoolExecutor extends AbstractExecutorService {
+    private final List<MyPoolThread> threads;
+    private final List<Runnable> tasks;
+    private MyPoolState state;
+
+    public MyPoolExecutor(int corePoolSize) {
+        threads = new ArrayList<>();
+        tasks = new LinkedList<>();
+        state = MyPoolState.RUNNING;
+
+        for (int i = 0; i < corePoolSize; i++) {
+            threads.add(new MyPoolThread(tasks));
+        }
+        threads.stream().forEach(Thread::start);
+    }
+
+    @Override
+    public void shutdown() {
+        state = MyPoolState.TERMINATED;
+
+        threads.stream().forEach(myPoolThread -> {
+            try {
+                System.out.println(myPoolThread + " shutdown 1");
+                myPoolThread.join();
+                System.out.println(myPoolThread + " shutdown 2");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        state = MyPoolState.SHUTDOWN;
+
+        threads.stream().forEach(Thread::interrupt);
+        return tasks;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return (state == MyPoolState.SHUTDOWN);
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return (state == MyPoolState.TERMINATED);
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (state == MyPoolState.RUNNING) {
+            synchronized (tasks){
+                tasks.add(command);
+            }
+        } else {
+            throw new IllegalStateException(String.format("Состояние пула %s не допускает добавление новых задач.", state));
+        }
+    }
+}
